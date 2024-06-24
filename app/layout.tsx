cd284ef8fc5efab8559a543d919667b9cbabaa97
lisonallen <lisonallen@qq.com>
2024-06-24 11:32:10 +0800

    update favicon.ico;

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,6 +33,10 @@ export default function RootLayout({
         <head>
             <meta name="google-site-verification" content="rpDVApX0PnUjDfaTwh0tke808LErmuAljbtB7UMvyME"/>
             <link rel="preload" href="/img/thumbnail_01.webp" as="image"/>
+            <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+            <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+            <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+            <link rel="manifest" href="/site.webmanifest"/>
             <Script
                 src={`https://www.googletagmanager.com/gtag/js?id=${gaIds[0]}`}
                 strategy="afterInteractive"
